a08ab375cc7af8067168adc40ab5865547d76059
JayPanchalEZ <panchalkalu643@gmail.com>
2023-09-14 18:34:09 +0530

    Second Commit

--- app/src/main/java/com/example/peaceofmind/RegisterPage.kt
+++ app/src/main/java/com/example/peaceofmind/RegisterPage.kt
@@ -1,8 +1,6 @@
 package com.example.peaceofmind
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,11 +8,11 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
 
 class RegisterPage : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
@@ -28,7 +26,7 @@ class RegisterPage : AppCompatActivity() {
             val password = findViewById<EditText>(R.id.password_register).text.toString()
 
             auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this){task ->
+                .addOnCompleteListener(this){task->
                     if(task.isSuccessful){
                         Toast.makeText(this, "Registration Successful!", Toast.LENGTH_SHORT).show()
                         val intent = Intent(this, LoginPage::class.java)
@@ -36,14 +34,16 @@ class RegisterPage : AppCompatActivity() {
                         finish()
                     }
                     else{
-                        Toast.makeText(this, "Registration failed. Please try again.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Registration Failed! Please Try Again", Toast.LENGTH_SHORT).show()
                     }
                 }
         }
-        val loginLink = findViewById<TextView>(R.id.have_acc_login)
-        loginLink.setOnClickListener {
+        val LoginLink =findViewById<TextView>(R.id.have_acc_register)
+
+        LoginLink.setOnClickListener{
             val intent = Intent(this, LoginPage::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
