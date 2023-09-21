2cd55b4f6982b1e685822da269ec2fc27316d154
JayPanchalEZ <panchalkalu643@gmail.com>
2023-09-22 00:27:02 +0530

    Fourth Commit

--- app/src/main/java/com/example/peaceofmind/Fragments/MeditateFragment.kt
+++ app/src/main/java/com/example/peaceofmind/Fragments/MeditateFragment.kt
@@ -1,60 +1,43 @@
 package com.example.peaceofmind.Fragments
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import com.example.peaceofmind.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MeditateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MeditateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meditate, container, false)
+        val view = inflater.inflate(R.layout.fragment_meditate, container, false)
+
+        val YoutubeButton = view.findViewById<Button>(R.id.YoutubeButton)
+        val image = view.findViewById<ImageView>(R.id.ImageLink)
+
+        YoutubeButton.setOnClickListener {
+
+            val url = "https://youtu.be/mZxcw2rPWxU?si=X73o72NzyuYjuRTn"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+        image.setOnClickListener{
+            val imageLink = "https://www.mindful.org/how-to-meditate/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(imageLink))
+            startActivity(intent)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MeditateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MeditateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
